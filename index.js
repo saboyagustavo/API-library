a8d130239634bb05a0b4985462d8b1cb1b87f52e
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-09-10 15:17:52 -0300

    Implemented the app and the lowDb.

---
+++ index.js
@@ -0,0 +1,33 @@
+import { join, dirname } from 'path';
+import { Low, JSONFileSync } from 'lowdb';
+import { fileURLToPath } from 'url';
+
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+//Use JSON file for storage
+const dbFile = join(__dirname, 'db.json');
+const adapter = new JSONFileSync(dbFile);
+const database = new Low(adapter);
+
+//Read data from JSON file, this will set db.data content
+await database.read();
+
+//If file.json doesn't exists, db.data will be null
+//So set the default data
+database.data = database.data || { books: [] };
+
+await database.write();
+
+const app = express();
+
+app.db = database;
+app.use(cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+const PORT = process.env.PORT || 4444;
+app.listen(PORT, () => console.log(`The server is running on port ${PORT}`));
