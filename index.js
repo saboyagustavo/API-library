4c6138f7d944c3eb28ecfa2c906d322f25db8265
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-09-11 14:31:50 -0300

    Configuring the swagger-jsdoc and swaggerUI.

--- index.js
+++ index.js
@@ -2,8 +2,31 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import booksRouter from './routes/books.js';
+import swaggerUI from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+import swaggerJSDoc from 'swagger-jsdoc';
+
+const optionsJsdoc = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Library API',
+      version: '1.0.0',
+      description: 'A simple Express Library API',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4444',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specsDocs = swaggerJSDoc(optionsJsdoc);
 
 const app = express();
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specsDocs));
 
 app.use(cors());
 app.use(express.json());
